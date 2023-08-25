6d892f8325e275a8efa8711a0cef0d2301240dbf
zhakyp02 <131019409+zhakyp02@users.noreply.github.com>
2023-08-25 23:19:39 +0600

    Update main.tf

--- readreplica-root/main.tf
+++ readreplica-root/main.tf
@@ -1,5 +1,5 @@
 #  If you are using a different repository for 'rr', 
-#  you can comment it out and replace the data terraform_remote_state with from your 
+#  you can comment it out data terraform_remote_state and replace  with from your 
 #  master instance's state file, such as the bucket name and prefix.
 
 # If you are using the same module as your master instance, 
